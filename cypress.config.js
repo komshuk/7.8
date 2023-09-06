5f87615e00512527d54c4916098fe124649e6e2c
komshuk <komshuk@yandex.ru>
2023-09-06 21:02:57 +0300

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,33 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'j81xst',
+  projectId: 'jio4o9',
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+        console.log(launchOptions.args) // print all current args
+
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          // auto open devtools
+          launchOptions.args.push('--auto-open-devtools-for-tabs')
+        }
+
+        if (browser.family === 'firefox') {
+          // auto open devtools
+          launchOptions.args.push('-devtools')
+        }
+
+        if (browser.name === 'electron') {
+          // auto open devtools
+          launchOptions.preferences.devTools = true
+        }
+
+        // whatever you return here becomes the launchOptions
+        return launchOptions
+      })
     },
   },
-});
+})
